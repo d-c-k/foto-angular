d6826342408d7fccf7589cd9dab559553302a08a
Daniel Koefoed <dc.koefoed@gmail.com>
2021-09-22 19:48:07 +0200

    generating display-slides from post-mockup

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from '../../assets/post';
+import { POSTS } from '../../assets/post-mockup';
+
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss']
 })
 export class DisplayComponent implements OnInit {
+    posts = POSTS;
 
   constructor() { }
 
--- src/assets/post-mockup.ts
+++ src/assets/post-mockup.ts
@@ -2,7 +2,7 @@ import { Post } from './post';
 
 export const POSTS: Post[] = [
     {
-        id: 1,
+        id: 0,
         date: new Date(),
         author: 'Daniel Koefoed',
         info: 'Rulle plåtad i augusti 21, Kodak 400 i en Minolta SRT 120', 
@@ -11,12 +11,12 @@ export const POSTS: Post[] = [
         ]
     },
     {
-        id: 2,
+        id: 1,
         date: new Date(),
         author: 'Olof Koefoed',
         info: 'Rulle plåtad i augusti 31, Kodak 200 i en Minolta SRT 120 b', 
         images: [
-            '../../assets/images/R1-07795-014A.jpg'
+            '../../assets/images/R1-06485-026A.jpg'
         ]
     }
 ]
